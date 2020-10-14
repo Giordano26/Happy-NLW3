f93ab7d5af78beed307d52d907ec62d904c3beca
Stefano <smfgiordano@gmail.com>
2020-10-14 13:03:19 -0300

    First Commit Day 1

---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+import Landing from './pages/landing'
+import OrfanatosMap from './pages/orfanatosMap'
+
+function Routes() {
+    return(
+        <BrowserRouter>
+        <Switch>
+            <Route path ="/" exact component={Landing} />
+            <Route path="/app" component={OrfanatosMap} />
+        </Switch>
+        </BrowserRouter>
+
+    );
+}
+
+export default Routes;
